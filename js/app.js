8e9e3740d62894b66bb23aa160a5fd469260b73a
Joakim Svensson <svensson.joakim@gmail.com>
2018-05-22 22:59:29 +0200

    Add keycode finder

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('Loading page')
+//#region keycode
+const keyCodeDisplay = document.querySelector('#keycode')
+const keyDisplay = document.querySelector('#pressedkey')
+
+function setKeyInfo(e) {
+    keyCodeDisplay.innerHTML = e.keyCode;
+    keyDisplay.innerHTML = `<div>${e.key}</div>`;
+}
+window.addEventListener('keydown', setKeyInfo)
+//#endregion
